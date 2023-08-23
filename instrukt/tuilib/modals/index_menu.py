00f8d9270af748cf1024327860b42247b8821379
blob42 <contact@blob42.xyz>
2023-08-23 17:32:51 +0200

    index dropdown menu: async instertion of indexes as retrieval tool

--- instrukt/tuilib/modals/index_menu.py
+++ instrukt/tuilib/modals/index_menu.py
@@ -21,6 +21,7 @@
 """Index menu modal"""
 
 import typing as t
+from asyncio import create_task
 
 from textual import on
 from textual.app import ComposeResult
@@ -30,12 +31,14 @@ from textual.reactive import reactive
 from textual.widgets import Button, SelectionList
 from textual.widgets.selection_list import Selection
 
+from ...context import index_manager
 from ...types import InstruktDomNodeMixin
 from ...views.index import IndexScreen
 from .basemenu import BaseModalMenu
 
 if t.TYPE_CHECKING:
     from ...indexes.schema import Collection
+    from ...app import InstruktApp
 
 
 class IndexMenuScreen(BaseModalMenu, InstruktDomNodeMixin):
@@ -47,7 +50,7 @@ class IndexMenuScreen(BaseModalMenu, InstruktDomNodeMixin):
     def compose(self) -> ComposeResult:
 
         with Container(id="menu"):
-            yield Button("Manage", id="manage", variant="warning")
+            yield Button("Manage", id="manage", variant="default")
             yield from self._build_menu()
 
     def index_attached_as_tool(self, index: str) -> bool:
@@ -73,31 +76,30 @@ class IndexMenuScreen(BaseModalMenu, InstruktDomNodeMixin):
 
     async def _update_menu(self) -> None:
         await self.query_one(SelectionList).remove()
-        await self.query_one("Container#menu").mount(*self._build_menu())
+        await self.query_one("Container#menu").mount_all(self._build_menu())
         self.query_one(SelectionList).focus()
 
 
-
-
-    def add_index_as_tool(self, index_name: str) -> None:
+    async def add_index_as_tool(self, index_name: str) -> None:
         """Add selected index as tool to the active agent."""
-        # get selected index's Index object
-        im = self._app.context.index_manager
-        index = im.get_index(index_name)
-        if index is None:
-            self._app.context.error(f"Index {index_name} not found")
-            return
+        async with self.app._alock:
+            with index_manager() as im:
+                index = await im.aget_index(index_name)
+                if index is None:
+                    self._app.context.error(f"Index {index_name} not found")
+                    return
+                # get tool from index, return_direct
+                tool = index.get_retrieval_tool(return_direct=True,
+                                        with_sources=True)
 
-        # get tool from index, return_direct
-        tool = index.get_retrieval_tool(return_direct=True)
+                # add tool to active agent
+                agent = self._app.agent_manager.active_agent
+                if agent is None:
+                    self._app.context.error("No active agent")
+                    return
 
-        # add tool to active agent
-        agent = self._app.agent_manager.active_agent
-        if agent is None:
-            self._app.context.error("No active agent")
-            return
-
-        agent.add_tool(tool)
+                agent.add_tool(tool)
+                self._app.context.info(f"Added index <{index_name}> as tool")
 
 
     def detach_index_tool(self, index_name: str) -> None:
@@ -111,25 +113,24 @@ class IndexMenuScreen(BaseModalMenu, InstruktDomNodeMixin):
         changed=False
         self.log.debug("sync collections")
         # check if there are new collections and update local one
-        im = self._app.context.index_manager
-        assert im is not None, "No index manager"
-        # compare im.indexes with self.collections and update self.collections
-        for index in im.indexes:
-            if index not in self.collections:
-                self.collections[index] = im.get_index(index)
-                changed = True
+        async with self.app._alock:
+            with index_manager() as im:
+                for index in im.indexes:
+                    if index not in self.collections:
+                        self.collections[index] = im.get_index(index)
+                        changed = True
 
-        _to_remove = []
-        for c in self.collections.keys():
-            if c not in im.indexes:
-                _to_remove.append(c)
-                changed=True
+                _to_remove = []
+                for c in self.collections.keys():
+                    if c not in im.indexes:
+                        _to_remove.append(c)
+                        changed=True
 
-        for c in _to_remove:
-            del self.collections[c]
+                for c in _to_remove:
+                    del self.collections[c]
 
-        if changed:
-            await self._update_menu()
+                if changed:
+                    await self._update_menu()
 
 
     @on(ScreenResume)
@@ -157,7 +158,7 @@ class IndexMenuScreen(BaseModalMenu, InstruktDomNodeMixin):
         self.app.push_screen(IndexScreen())
 
     @on(SelectionList.SelectionToggled)
-    def update_selected_index(self, s: SelectionList.SelectionMessage) -> None:
+    async def update_selected_index(self, s: SelectionList.SelectionMessage) -> None:
         self.log.info("Selection toggled")
         # self.log.debug(s.selection.value)
         # self.log.debug(s.selection.prompt)
@@ -168,7 +169,11 @@ class IndexMenuScreen(BaseModalMenu, InstruktDomNodeMixin):
         # selected index: inject as tool
         if s.selection.value in s.selection_list.selected:
             self.log.debug("selected")
-            self.add_index_as_tool(s.selection.value)
+            self.call_next(self.add_index_as_tool, s.selection.value)
+            self.dismiss()
+
+
+
 
         # deselected: detach underlying tool if attached
         else:
