5eb94930df1c1dcdeabc137a90356e6556798f63
blob42 <contact@blob42.xyz>
2023-08-25 19:43:50 +0200

    wip programming assistant

---
+++ instrukt/agent_modules/coding_ai/main.py
@@ -0,0 +1,55 @@
+from typing import TYPE_CHECKING, Optional
+
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
+
+from instrukt.agent.base import InstruktAgent
+from instrukt.config import APP_SETTINGS
+from instrukt.output_parsers.multi_strategy import multi_parser
+from instrukt.tools.base import TOOL_REGISTRY
+
+from .prompt import PREFIX
+
+if TYPE_CHECKING:
+    from instrukt.context import Context
+
+
+class CodingAI(InstruktAgent):
+
+    name = "coding_ai"
+    description = "A conversational assistant optimized for technical and programming tasks."
+    display_name = "Coding AI"
+
+    @classmethod
+    def load(cls, ctx: 'Context') -> Optional[InstruktAgent]:
+        """Load the agent."""
+
+        llm = ChatOpenAI(**APP_SETTINGS.openai.dict())
+        llm.model_name="gpt-4"
+        agent_kwargs = dict(
+            system_message=PREFIX,
+            output_parser= multi_parser,
+        )
+        executor_params = dict(
+            agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+            max_iterations=7,
+            agent_kwargs=agent_kwargs,
+        )
+
+        wiki_tool = TOOL_REGISTRY.tools["Wikipedia"]
+        wiki_tool.attached = False
+        toolset = (
+            wiki_tool,
+            # INSERT TOOLS HERE
+        )
+
+        instrukt_agent = cls(toolset=toolset,
+                             llm=llm,
+                             executor_params=executor_params)
+
+        return instrukt_agent
+
+    @classmethod
+    async def aload(cls, ctx: 'Context') -> Optional[InstruktAgent]:
+        raise NotImplementedError("Use load instead")
+        return None
---
+++ instrukt/agent_modules/coding_ai/prompt.py
@@ -0,0 +1,33 @@
+##
+##  Copyright (c) 2023 Chakib Ben Ziane <contact@blob42.xyz> . All rights reserved.
+##
+##  SPDX-License-Identifier: AGPL-3.0-or-later
+##
+##  This file is part of Instrukt.
+## 
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU Affero General Public License as
+##  published by the Free Software Foundation, either version 3 of the
+##  License, or (at your option) any later version.
+## 
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU Affero General Public License for more details.
+## 
+##  You should have received a copy of the GNU Affero General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+PREFIX = """You are Bob, a knowledgeable IT person with expert knowledge in programming matters. You are based on a large language model that has been trained on a diverse range of coding-related information. Your role is to act as a coding assistant and help users understand programming concepts, brainstorm ideas, and explore different topics. You may have access to a variety of tools and knowledge sources to assist you in your role.
+
+Conversation Guidelines:
+1. Users can ask you factual questions about programming languages, syntax, algorithms, data structures, coding best practices, and other related topics. If you don't know the answer to a question, please clearly state so and DO NOT make up answers.
+2. Users can seek your guidance on problem-solving approaches, debugging techniques, and optimizing code efficiency. Let's think step by step to explore different potential solutions.
+3. Users can discuss their project ideas with you and ask for suggestions or feedback. Feel free to provide insights and recommendations based on your expertise.
+4. Users can request assistance with specific coding problems or errors they encounter. Let's analyze the code together to identify the issue and find a solution.
+5. Users may provide you with specific tools or knowledge sources to incorporate into your responses. Please respect the input format and utilize the provided tools effectively.
+6. You should provide clear and concise explanations, examples, and code snippets whenever necessary to aid user understanding.
+7. You should maintain a friendly and professional tone throughout the conversation.
+
+Please adhere to the guidelines above and make sure to provide accurate and reliable information."""
